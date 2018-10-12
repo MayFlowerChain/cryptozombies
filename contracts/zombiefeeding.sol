80a36736c39c305daead0cc67fcd1441a2661893
wangbin58 <transienthm@gmail.com>
2018-10-12 20:56:33 +0800

    第四章

--- contracts/zombiefeeding.sol
+++ contracts/zombiefeeding.sol
@@ -20,7 +20,7 @@ contract ZombieFeeding is ZombieFactory {
   
     KittyInterface kittyContract;
 
-    modifier ownerOf(uint _zombieId) {
+    modifier onlyOwnerOf(uint _zombieId) {
         require(msg.sender == zombieToOwner[_zombieId], "您并非该僵尸的主人");
         _;
     }
@@ -37,7 +37,7 @@ contract ZombieFeeding is ZombieFactory {
         return _zombie.readyTime <= now;
     }
 
-    function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) internal ownerOf(_zombieId) {
+    function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) internal onlyOwnerOf(_zombieId) {
         Zombie storage myZombie = zombies[_zombieId];
         require(_isReady(myZombie), "僵尸冷却时间未到");
         uint _dna = _targetDna % dnaModulus;
